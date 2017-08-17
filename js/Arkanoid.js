86f26e1bfa2d34b2f65e7e6e50610af5f9386815
ignaciocasales <casales.ignacio@gmail.com>
2017-08-17 16:09:58 -0300

    Issue #1 Vidas y Puntaje

--- js/Arkanoid.js
+++ js/Arkanoid.js
@@ -5,11 +5,11 @@
  * @type Object
  */
 const Movement = {
-	NONE : 0,
-	LEFT : 1,
-	RIGHT : 2,
-	UP : 4,
-	DOWN : 8
+	NONE: 0,
+	LEFT: 1,
+	RIGHT: 2,
+	UP: 4,
+	DOWN: 8
 };
 
 /**
@@ -53,10 +53,11 @@ function Arkanoid(canvas) {
 	const VAUS_HEIGHT = 10;
 	const BULLET_SIZE = 3;
 	const BULLET_MAX_SPEED = 20;
+	let LIVES = 3;
+	let SCORE = 0;
 
-	this.init = function() {
-		if(!canvas.getContext)
-		{
+	this.init = function () {
+		if (!canvas.getContext) {
 			console.warn('Tu navegador no soporta canvas');
 			return;
 		}
@@ -69,28 +70,29 @@ function Arkanoid(canvas) {
 
 		setInterval(() => {
 			this.update();
+			this.updateScore();
 			this.fullDraw();
 		}, 20);
 	}
 
-	this.createElements = function() {
+	this.createElements = function () {
 		this.vaus = new Vaus(canvas.width / 2 - VAUS_WIDTH / 2, canvas.height - 20, VAUS_WIDTH, VAUS_HEIGHT);
 		this.bullet = new Bullet(canvas.width / 2, canvas.height / 2, BULLET_SIZE, Movement.NONE);
 	}
 
-	this.drawBullet = function() {
+	this.drawBullet = function () {
 		this.context.beginPath();
 		this.context.arc(this.bullet.x, this.bullet.y, this.bullet.radius, 0, 2 * Math.PI, false);
 		this.context.fillStyle = 'yellow';
 		this.context.fill();
 	}
 
-	this.drawVaus = function() {
+	this.drawVaus = function () {
 		this.context.fillStyle = 'rgb(155,110,5)';
 		this.context.fillRect(this.vaus.x, this.vaus.y, this.vaus.width, this.vaus.height);
 	}
 
-	this.fullDraw = function() {
+	this.fullDraw = function () {
 		this.context.clearRect(0, 0, canvas.width, canvas.height);
 
 		canvas.style.cursor = "none";
@@ -101,22 +103,26 @@ function Arkanoid(canvas) {
 
 		this.drawVaus();
 
-		if(this.gameOver)
-			this.writeText('Game Over');
+		this.writeText('Lives: ' + parseInt(LIVES), 10, 20);
+
+		this.writeText('Score: ' + parseInt(SCORE), 10, 40);
+
+		if (this.gameOver)
+			this.writeText('Game Over', canvas.width / 2 - 40, canvas.height / 2);
 	}
 
-	this.update = function() {
-		if (this.pause || this.gameOver) 
+	this.update = function () {
+		if (this.pause || this.gameOver)
 			return;
 
-		if (this.bullet.dir & Movement.RIGHT) 
+		if (this.bullet.dir & Movement.RIGHT)
 			this.bullet.x += this.bullet.speed;
-		else if (this.bullet.dir & Movement.LEFT) 
+		else if (this.bullet.dir & Movement.LEFT)
 			this.bullet.x -= this.bullet.speed;
 
 		if (this.bullet.dir & Movement.UP)
 			this.bullet.y -= this.bullet.speed;
-		else if (this.bullet.dir & Movement.DOWN) 
+		else if (this.bullet.dir & Movement.DOWN)
 			this.bullet.y += this.bullet.speed;
 
 		if ((this.bullet.x + this.bullet.radius > this.vaus.x && this.bullet.x - this.bullet.radius < this.vaus.x + this.vaus.width) &&
@@ -126,7 +132,7 @@ function Arkanoid(canvas) {
 			if (this.bullet.dir & Movement.DOWN) {
 				this.bullet.dir = this.bullet.dir - Movement.DOWN + Movement.UP;
 			} else if (this.bullet.dir & Movement.UP) {
-			 	this.bullet.dir = this.bullet.dir - Movement.UP + Movement.DOWN;
+				this.bullet.dir = this.bullet.dir - Movement.UP + Movement.DOWN;
 			}
 		}
 
@@ -145,7 +151,12 @@ function Arkanoid(canvas) {
 
 		if (this.bullet.y + this.bullet.radius > canvas.height) {
 			this.bullet.speed = 3;
-			this.gameOver = true;
+			LIVES = LIVES - 1;
+			if (LIVES > 0) {
+				this.restartGame();
+			} else if (LIVES <= 0) {
+				this.gameOver = true;
+			}
 		}
 
 		if (this.bullet.dir == Movement.NONE) {
@@ -154,19 +165,31 @@ function Arkanoid(canvas) {
 		}
 	}
 
-	this.writeText = function(text) {
-		this.context.fillStyle = 'rgb(255,255,0)';
-		this.context.font = 'bold 20px Arial';
-		this.context.fillText(text, canvas.width / 2 - 40, canvas.height / 2);
+	this.updateScore = function () {
+		if (this.bullet.dir != Movement.NONE) {
+			SCORE += 1;
+		}
 	}
 
-	this.startGame = function() {
+	this.writeText = function (text, width, height) {
+		this.context.fillStyle = 'rgb(255,255,0)';
+		this.context.font = 'bold 20px Arial';
+		this.context.fillText(text, width, height);
+	}
+
+	this.startGame = function () {
 		if (this.bullet.dir == Movement.NONE) {
 			this.bullet.dir = Movement.RIGHT + Movement.UP;
 		}
 	}
 
-	this.setVausPosition = function(x) {
+	this.restartGame = function () {
+		this.bullet.dir = Movement.NONE;
+		this.bullet.x = this.vaus.x + this.vaus.width / 2;
+		this.bullet.y = this.vaus.y - this.bullet.radius * 2;
+	}
+
+	this.setVausPosition = function (x) {
 		if (this.pause || this.gameOver) return;
 		if (x < 0) x = 0;
 		if (x > canvas.width - this.vaus.width) x = canvas.width - this.vaus.width;
